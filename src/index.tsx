c6786d965719ce3e95194ba2d1d61bc61859cf13
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-12-15 23:44:22 +0900

    ⭐ feat : GlobalStyle, theme 설정 1. index.html : 구글 폰트 추가 2. GlobalStyle : css 속성들 reset, 폰트 설정 3. theme : fontSize, color, breakpoint 설정, 타입 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import  theme from './styles/theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
 
